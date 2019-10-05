44bd82edd51f2ad546e3547962170ebfc141c53b
Daniel Lovhaug <46870587+lovhaugd@users.noreply.github.com>
2019-10-05 11:34:29 -0500

    Create loadMockData.sql
    Added on behalf of Vishal

---
+++ INSTALL/loadMockData.sql
@@ -0,0 +1,23 @@
+INSERT INTO Address (AddressId, AddressLine1, City, State, ZipCodePt1)
+    VALUE
+    (1, '2345 vip st', 'new york', 'NY', 44423),
+    (2, '9845 Broadway Ave', 'Chicago', 'IL', 45331),
+    (3, '5423 vip st', 'Duluth', 'MN', 15623),
+    (4, '3212 vip st', 'Dallas', 'TX', 54128),
+    (5, '6565 vip st', 'Arizona', 'AZ', 24151);
+
+INSERT INTO Category (CategoryId, Description)
+    VALUE
+    (1, 'General'),
+    (2, 'Home'),
+    (3, 'Outdoor'),
+    (4, 'Health'),
+    (5, 'Education');
+
+INSERT INTO ContactInfo (ContactInfoId, PhoneNumber, EmailAddress)
+    VALUE
+    (1, '743-242-5632', 'xyz@gmail.com'),
+    (2, '645-487-5432', 'Allena_12@gmail.com'),
+    (3, '453-555-1642', 'Eric65@gmail.com'),
+    (4, '332-231-5241', 'Dalen98@gmail.com'),
+    (5, '433-142-6942', 'Vishal@gmail.com');
