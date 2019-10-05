df68b1f741a48235ed88217daeb7f0a2a9b8694b
Daniel Lovhaug <46870587+lovhaugd@users.noreply.github.com>
2019-10-05 13:49:04 -0500

    Create navigationBar.php

---
+++ components/navigationBar.php
@@ -0,0 +1,9 @@
+<header class="main-header">
+	<div class="container">
+		<nav class="navbar navbar-static-top">
+			<div class="container">
+				NAVIGATION BAR
+			</div>
+		</nav>
+	</div>
+</header>
