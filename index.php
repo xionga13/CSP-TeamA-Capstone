9d16aa21a026299028e3ad75ce2491379321c456
Daniel Lovhaug <46870587+lovhaugd@users.noreply.github.com>
2019-10-05 12:44:16 -0500

    Added db connection and query testing

--- index.php
+++ index.php
@@ -15,6 +15,14 @@ include('components/session.php');
 
 	<?php
 	echo "Hello World!";
+	$sql = file_get_contents('dataAccess/sql/TEST.sql');
+	$conn = $pdo->open();
+	$result = $conn->prepare($sql);
+	$result->execute();
+	foreach ($result as $row) {
+		echo "<p>" . $row['TABLE_TYPE'] . "</p>";
+	}
+	$pdo->close();
 	?>
 
 </body>
