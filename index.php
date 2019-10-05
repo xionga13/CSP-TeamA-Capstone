eb76481fbe2ae0ee003b960dec257ed91a7165ab
Daniel Lovhaug <46870587+lovhaugd@users.noreply.github.com>
2019-10-05 13:50:14 -0500

    added header, footer, nav bar, and scripts refs

--- index.php
+++ index.php
@@ -1,29 +1,24 @@
-<?php
-include('components/session.php');
-?>
-<!DOCTYPE html>
-<html>
-<head>
-
-  	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-  	<title>Ol' General Store</title>
-	
-</head>
+<?php include('/components/session.php'); ?>
+<?php include '/components/header.php'; ?>
 <body>
-	<h1>Testing Page</h1>
+	<div class="container">
+		<?php include '/components/navigationBar.php'; ?>
+		<h1>Testing Page</h1>
 
-	<?php
-	echo "Hello World!";
-	$sql = file_get_contents('dataAccess/sql/TEST.sql');
-	$conn = $pdo->open();
-	$result = $conn->prepare($sql);
-	$result->execute();
-	foreach ($result as $row) {
-		echo "<p>" . $row['TABLE_TYPE'] . "</p>";
-	}
-	$pdo->close();
-	?>
-
+		<?php
+		echo "Hello World!";
+		$sql = file_get_contents('dataAccess/sql/TEST.sql');
+		$conn = $pdo->open();
+		$result = $conn->prepare($sql);
+		$result->execute();
+		foreach ($result as $row) {
+			echo "<p>" . $row['TABLE_TYPE'] . "</p>";
+		}
+		$pdo->close();
+		?>
+		
+		<?php include '/components/footer.php'; ?>
+	</div>
+	<?php include '/components/scripts.php'; ?>
 </body>
 </html>
