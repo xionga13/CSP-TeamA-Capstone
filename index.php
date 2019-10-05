dd4aa23c865c6d45519981ff06e3d2d6fef1681c
Daniel Lovhaug <46870587+lovhaugd@users.noreply.github.com>
2019-10-05 11:49:36 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+include('components/session.php');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+
+  	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+  	<title>Ol' General Store</title>
+	
+</head>
+<body>
+	<h1>Testing Page</h1>
+
+	<?php
+	echo "Hello World!";
+	?>
+
+</body>
+</html>
